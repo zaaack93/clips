2ae19b541273d8605e45fba12b34519c2ab2cbdb
zbouzangad <bouzanagdzakaria@gmail.com>
2022-12-16 21:28:34 +0100

    the content children decorator

--- src/app/shared/container-tabs/container-tabs.component.ts
+++ src/app/shared/container-tabs/container-tabs.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ContentChildren, AfterContentInit } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-container-tabs',
   templateUrl: './container-tabs.component.html',
-  styleUrls: ['./container-tabs.component.css']
+  styleUrls: ['./container-tabs.component.css'],
 })
-export class ContainerTabsComponent {
+export class ContainerTabsComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs = {};
 
+  //we use this instead of init because we want the content tabs initialised
+  ngAfterContentInit(): void {
+    console.log(this.tabs);
+  }
 }
