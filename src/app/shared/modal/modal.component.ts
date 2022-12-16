fef3f4df9aed885d5c77cb5f454a9ed5834c1306
zbouzangad <bouzanagdzakaria@gmail.com>
2022-12-16 21:01:33 +0100

    working on teleport modal to root component

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, ElementRef } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -6,12 +6,15 @@ import { ModalService } from 'src/app/services/modal.service';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
   @Input() modalID: string = '';
 
-  constructor(public modal: ModalService) {}
+  constructor(public modal: ModalService, public el: ElementRef) {}
 
   closeModal() {
     this.modal.toggleModalVisible(this.modalID);
+  }
+  ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
   }
 }
