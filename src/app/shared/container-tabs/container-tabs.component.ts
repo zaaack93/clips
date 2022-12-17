4ae66ae079d8bd1cfb2057b367984af4ce8f4e64
zbouzangad <bouzanagdzakaria@gmail.com>
2022-12-17 14:56:41 +0100

    setting an active tab

--- src/app/shared/container-tabs/container-tabs.component.ts
+++ src/app/shared/container-tabs/container-tabs.component.ts
@@ -17,6 +17,21 @@ export class ContainerTabsComponent implements AfterContentInit {
 
   //we use this instead of init because we want the content tabs initialised
   ngAfterContentInit(): void {
-    console.log(this.tabs);
+    //for the first cas we need to set an active tab for default ? is option if
+
+    var activeTabs = this.tabs?.filter((tab) => tab.active);
+
+    if (!activeTabs || activeTabs.length === 0) {
+      // ! we have confident this propertie will have at least one item
+      this.setActiveTab(this.tabs!.first);
+    }
+  }
+
+  setActiveTab(tab: TabComponent) {
+    //set all the tabs inactive
+    this.tabs.forEach((tab) => {
+      tab.active = false;
+    });
+    tab.active = true;
   }
 }
