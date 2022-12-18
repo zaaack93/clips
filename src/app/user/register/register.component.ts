289e39d800fa40d4cd0480d4320b373addd6509b
zbouzangad <bouzanagdzakaria@gmail.com>
2022-12-18 14:28:28 +0100

    working on register form // add first validation

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-
+  RegisterForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    age: new FormControl('', [Validators.required, Validators.min(0)]),
+    password: new FormControl(''),
+    confirm_password: new FormControl(''),
+    phoneNumber: new FormControl(''),
+  });
 }
