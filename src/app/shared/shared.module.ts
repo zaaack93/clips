d575fad0a0de17226a9c026bfaa06fe65159f498
zbouzangad <bouzanagdzakaria@gmail.com>
2022-12-18 15:13:12 +0100

    working on Input component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,22 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabComponent } from './tab/tab.component';
 import { ContainerTabsComponent } from './container-tabs/container-tabs.component';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ModalComponent, TabComponent, ContainerTabsComponent],
-  imports: [CommonModule],
-  exports: [ModalComponent, TabComponent, ContainerTabsComponent],
+  declarations: [
+    ModalComponent,
+    TabComponent,
+    ContainerTabsComponent,
+    InputComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [
+    ModalComponent,
+    TabComponent,
+    ContainerTabsComponent,
+    InputComponent,
+  ],
 })
 export class SharedModule {}
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -7,12 +7,18 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  name = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  age = new FormControl('', [Validators.required, Validators.min(0)]);
+  password = new FormControl('');
+  confirm_password = new FormControl('');
+  phoneNumber = new FormControl('');
   RegisterForm = new FormGroup({
-    name: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    age: new FormControl('', [Validators.required, Validators.min(0)]),
-    password: new FormControl(''),
-    confirm_password: new FormControl(''),
-    phoneNumber: new FormControl(''),
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirm_password: this.confirm_password,
+    phoneNumber: this.phoneNumber,
   });
 }
