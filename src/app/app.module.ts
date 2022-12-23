253968314ad232659366a345bc886dc34ba9e4cd
ZBOUZANGAD <bouzanagdzakaria@gmail.com>
2022-12-23 10:32:40 +0100

    integrating firebase SDK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { UserModule } from './user/user.module';
+import { AngularFireModule } from '@angular/fire/compat'
+import { AngularFireAuthModule } from '@angular/fire/compat/auth'
+import { environment } from 'src/environment/environment';
 
 @NgModule({
   declarations: [AppComponent, NavComponent],
-  imports: [BrowserModule, AppRoutingModule, UserModule],
+  imports: [BrowserModule, AppRoutingModule, UserModule,AngularFireModule.initializeApp(environment.firebase),AngularFireAuthModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBltK_RlqIFjRGVhJxSDUf9eLAHF91d3x0',
+    authDomain: 'clips-e105a.firebaseapp.com',
+    projectId: 'clips-e105a',
+    storageBucket: 'clips-e105a.appspot.com',
+    messagingSenderId: '625660488642',
+    appId: '1:625660488642:web:e097e22fde5c8967f9445b',
+  },
+};
