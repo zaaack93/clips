34676a286f94d6b9b6fe2fd7d2e9178bcd91e120
zbouzangad <bouzanagdzakaria@gmail.com>
2022-12-24 15:02:43 +0100

    Implimenting signout feature

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AuthService } from '../services/auth.service';
 import { ModalService } from '../services/modal.service';
 
@@ -8,9 +9,17 @@ import { ModalService } from '../services/modal.service';
   styleUrls: ['./nav.component.css'],
 })
 export class NavComponent {
-  constructor(public modal: ModalService, public auth: AuthService) {}
+  constructor(
+    public modal: ModalService,
+    public auth: AuthService,
+    private afauth: AngularFireAuth
+  ) {}
   openModal(event: Event) {
     event.preventDefault();
     this.modal.toggleModalVisible('auth');
+  }
+  async logout(event: Event) {
+    event.preventDefault();
+    await this.afauth.signOut();
   }
 }
