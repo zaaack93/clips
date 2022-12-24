9ac17cb3dbc81ffadc4c1a0104a60a22612542ab
zbouzangad <bouzanagdzakaria@gmail.com>
2022-12-24 14:36:57 +0100

    Destroying the modal after login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   ngOnInit() {}
+  constructor(public auth: AuthService) {}
 }
