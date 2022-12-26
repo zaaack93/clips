06f7dfe9ebf4980b152ea10d07c3fbd574a81a84
ZBOUZANGAD <bouzanagdzakaria@gmail.com>
2022-12-26 11:25:58 +0100

    Handling message error for password not match

--- src/app/user/validators/register-validators.ts
+++ src/app/user/validators/register-validators.ts
@@ -5,16 +5,21 @@ export class RegisterValidators {
     return (group: AbstractControl): ValidationErrors | null => {
       const control = group.get(controlName);
       const matchingControl = group.get(matchingControlName);
-      if (!control || !matchingControl)
+      if (!control || !matchingControl){
+        console.error('Form controls can not be found in the form group')
         return {
           controlNotFound: false,
         };
+      }
 
-      return control === matchingControl
+
+      const error= control.value === matchingControl.value
         ? null
         : {
             noMatch: true,
           };
+      matchingControl.setErrors(error)
+      return error
     };
   }
 }
