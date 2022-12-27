1dc82b25a4f807d39d189a7a8e4f16552ab0167a
ZBOUZANGAD <bouzanagdzakaria@gmail.com>
2022-12-27 11:32:51 +0100

    registre about route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [{path:"",component:HomeComponent}];
+const routes: Routes = [{path:"",component:HomeComponent},{path:"about",component:AboutComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
