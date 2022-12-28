c49960aa19ab8020c0bd3af3d229b360896741b1
ZBOUZANGAD <bouzanagdzakaria@gmail.com>
2022-12-28 12:14:41 +0100

    registring upload view with data router

--- src/app/video/video-routing.module.ts
+++ src/app/video/video-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManageComponent } from './manage/manage.component';
+import { UploadComponent } from './upload/upload.component';
 
-const routes: Routes = [{path:"manage",component:ManageComponent,data:{
-  authOnly:true
-}}];
+const routes: Routes = [
+  {
+    path: 'manage',
+    component: ManageComponent,
+    data: {
+      authOnly: true,
+    },
+  },
+  {
+    path: 'upload',
+    component: UploadComponent,
+    data: {
+      authOnly: true,
+    },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
