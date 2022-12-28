71e4907b62176f67ca41247db8e7577e903adbc4
ZBOUZANGAD <bouzanagdzakaria@gmail.com>
2022-12-28 13:41:35 +0100

    Wildcard Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ClipComponent } from './clip/clip.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [{path:"",component:HomeComponent},{path:"about",component:AboutComponent},{path:"clip/:id",component:ClipComponent}];
+const routes: Routes = [{path:"",component:HomeComponent},{path:"about",component:AboutComponent},{path:"clip/:id",component:ClipComponent},{path:"**",component:NotFoundComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
