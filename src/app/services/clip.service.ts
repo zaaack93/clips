677a9d46ae46fe79bec5ca519c1a957b5fe41426
ZBOUZANGAD <bouzanagdzakaria@gmail.com>
2023-01-03 15:19:58 +0100

    Quering the database

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -1,3 +1,5 @@
+import { switchMap } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Injectable } from '@angular/core';
 import {
   AngularFirestore,
@@ -5,16 +7,27 @@ import {
   DocumentReference,
 } from '@angular/fire/compat/firestore';
 import IClip from '../models/clip.model';
+import { tap, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ClipService {
   clipCollection: AngularFirestoreCollection<IClip>;
-  constructor(private db: AngularFirestore) {
+  constructor(private db: AngularFirestore,private auth:AngularFireAuth) {
     this.clipCollection = this.db.collection('clips');
   }
   createClip(clip: IClip): Promise<DocumentReference<IClip>> {
     return this.clipCollection.add(clip);
+  }
+  getUserClips(){
+    //switch map should be also send a observable
+    return this.auth.user.pipe(switchMap(user=>{
+      if(!user){
+        return of([])
+      }
+      const query = this.clipCollection.ref.where('uid','==',user.uid)
+      return query.get()
+    }))
   }
 }
--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ClipService } from 'src/app/services/clip.service';
 
 @Component({
   selector: 'app-manage',
@@ -12,8 +13,10 @@ export class ManageComponent implements OnInit {
     this.route.queryParams.subscribe((params: Params)=>{
       this.videoOrder=params['sort']==='2' ? params['sort'] : '1'
     })
+
+    this.clipService.getUserClips().subscribe(console.log)
   }
-  constructor(private router :Router,private route:ActivatedRoute){}
+  constructor(private router :Router,private route:ActivatedRoute,private clipService:ClipService){}
   sortChange(){
     this.router.navigate([],
     {
