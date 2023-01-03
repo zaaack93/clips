dc3d33688c467cc034b7aad8fe56789e64f2b519
zbouzangad <bouzanagdzakaria@gmail.com>
2023-01-03 20:24:10 +0100

    bind values to the form

--- src/app/video/edit-video/edit-video.component.ts
+++ src/app/video/edit-video/edit-video.component.ts
@@ -1,4 +1,12 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import IClip from 'src/app/models/clip.model';
 import { ModalService } from 'src/app/services/modal.service';
 
@@ -7,14 +15,29 @@ import { ModalService } from 'src/app/services/modal.service';
   templateUrl: './edit-video.component.html',
   styleUrls: ['./edit-video.component.css'],
 })
-export class EditVideoComponent implements OnInit, OnDestroy {
+export class EditVideoComponent implements OnInit, OnDestroy, OnChanges {
   @Input()
   activeCLip: IClip | null = null;
+  title = new FormControl(this.activeCLip?.title, [
+    Validators.required,
+    Validators.minLength(3),
+  ]);
+  editForm = new FormGroup({
+    name: this.title,
+  });
+  inSubmission: boolean = false;
   constructor(private modal: ModalService) {}
   ngOnInit(): void {
     this.modal.register('editClipModal');
   }
+  //we arnt't able to listen to the property when it change so you add this lifecycle hook
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.activeCLip) {
+      this.title.setValue(this.activeCLip.title);
+    }
+  }
   ngOnDestroy(): void {
     this.modal.unregister('editClipModal');
   }
+  editClip() {}
 }
