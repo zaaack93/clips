44bd26a16f4ccf0ce60370807e1274eb213b52d5
ZBOUZANGAD <bouzanagdzakaria@gmail.com>
2023-01-10 15:27:42 +0100

    set style without encapsulation

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,11 +1,12 @@
 import videojs from 'video.js';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { map } from 'rxjs';
 @Component({
   selector: 'app-clip',
   templateUrl: './clip.component.html',
-  styleUrls: ['./clip.component.css']
+  styleUrls: ['./clip.component.css'],
+  encapsulation:ViewEncapsulation.None
 })
 export class ClipComponent implements OnInit {
   id='';
