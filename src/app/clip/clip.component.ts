6284ce7365724210822861ada9bffef0e8f42605
ZBOUZANGAD <bouzanagdzakaria@gmail.com>
2023-01-10 13:40:44 +0100

    initialise the video player

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,3 +1,4 @@
+import videojs from 'video.js';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { map } from 'rxjs';
@@ -9,13 +10,14 @@ import { map } from 'rxjs';
 export class ClipComponent implements OnInit {
   id='';
   @ViewChild('videoPlayer',{static:true}) targer?: ElementRef;
+  player?:videojs.Player;
   constructor(public route:ActivatedRoute){
 
   }
 
   ngOnInit(): void {
+    this.player=videojs(this.targer?.nativeElement)
     this.route.params.subscribe((params : Params) => console.log(params['id']))
   }
   //ngafter init is run if the template is ready so you can use the view child for HTML elements in this function
-  ng
 }
