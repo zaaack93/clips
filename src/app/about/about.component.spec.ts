f3f5822b6b1041817788264633531c519a81ca7e
zbouzangad <bouzanagdzakaria@gmail.com>
2023-01-12 22:27:33 +0100

    angular text bed utility

---
+++ src/app/about/about.component.spec.ts
@@ -0,0 +1,10 @@
+import { TestBed } from '@angular/core/testing';
+import { AboutComponent } from './about.component';
+
+describe('About Component', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AboutComponent],
+    }).compileComponents();
+  });
+});
