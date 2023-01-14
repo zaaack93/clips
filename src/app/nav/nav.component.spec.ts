bc683e4b4fb811487db3bf49b797c5b8833bed31
zbouzangad <bouzanagdzakaria@gmail.com>
2023-01-14 13:18:58 +0100

    mock service for make a fake dependency

--- src/app/nav/nav.component.spec.ts
+++ src/app/nav/nav.component.spec.ts
@@ -1,16 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { NavComponent } from './nav.component';
 
 describe('NavComponent', () => {
   let component: NavComponent;
   let fixture: ComponentFixture<NavComponent>;
+  const mockAuthService = jasmine.createSpyObj(
+    'AuthService',
+    ['createUser', 'logout'],
+    {
+      isAuthenticated$: of(true),
+    }
+  );
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavComponent ]
-    })
-    .compileComponents();
+      declarations: [NavComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(NavComponent);
     component = fixture.componentInstance;
