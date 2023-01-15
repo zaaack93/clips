a4c0abfa26b32d265f556cf0feb9a9ef309554fc
zbouzangad <bouzanagdzakaria@gmail.com>
2023-01-15 16:35:00 +0100

    simulate logout with DOM

--- src/app/nav/nav.component.spec.ts
+++ src/app/nav/nav.component.spec.ts
@@ -35,4 +35,18 @@ describe('NavComponent', () => {
     const lougoutLink = fixture.debugElement.query(By.css('li:nth-child(3) a'));
     expect(lougoutLink).withContext('not logged in').toBeTruthy();
   });
+
+  it('should logout', () => {
+    const lougoutLink = fixture.debugElement.query(By.css('li:nth-child(3) a'));
+    //handle click by debug element
+    lougoutLink.triggerEventHandler('click');
+
+    //grab the service instance for now is for spy object
+    const service = TestBed.inject(AuthService);
+    //
+    expect(service.logout)
+      .withContext('could not click to logout link')
+      .toHaveBeenCalledTimes(1);
+    //expect(lougoutLink).withContext('not logged in').toBeTruthy();
+  });
 });
